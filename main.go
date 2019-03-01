1c81ef059aea8333be618424eb31a4e7ba17e484
tammarut <tammarut.nua@gmail.com>
2019-03-01 16:45:24 +0700

    main.go: edit config

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ func main() {
 	e := echo.New()
 
 	// Middleware
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{ //=> Custom log
+		Format: `[${time_rfc3339}]  status=${status}  ${method} ${host}${path}  ${latency_human} ${latency}` + "\n", //=> [2019-02-11T23:56:04+07:00]  status=200  GET localhost:1323/admin/main  0s
+	}))
 	e.Use(middleware.Recover())
 
 	// Routes
