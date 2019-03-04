23799b558849ad38b98811f1c8173237ad290b6e
tammarut <tammarut.nua@gmail.com>
2019-03-04 13:52:26 +0700

    main.go: try service POST/func saveList code on main.go

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/tammarut/todolist-recap/handler"
 )
 
+type List struct {
+	Topic string `json: "topic"`
+}
+
 func main() {
 	// Echo instance
 	e := echo.New()
 
-	// Middleware
+	// MiddlewareW
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{ //=> Custom log
 		Format: `[${time_rfc3339}]  status=${status}  ${method} ${host}${path}  ${latency_human} ${latency}` + "\n", //=> [2019-02-11T23:56:04+07:00]  status=200  GET localhost:1323/admin/main  0s
 	}))
@@ -21,7 +29,18 @@ func main() {
 
 	// Routes
 	e.GET("/", h.Hello)
+	e.POST("/todos", saveList)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func saveList(c echo.Context) error {
+	list := new(List)
+	if err := c.Bind(list); err != nil {
+		log.Println("Error: from saveList", err)
+		c.String(http.StatusInternalServerError, "Error: from c.Bind func saveList")
+	}
+	fmt.Printf("%v#\n", list)
+	return c.JSON(http.StatusCreated, list)
 }
